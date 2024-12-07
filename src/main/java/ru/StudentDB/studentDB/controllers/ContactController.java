3dcf4d3ebe3a2e11fd85eb341e2ee7400e40fb20
xfetus <140549518+rkxrichard@users.noreply.github.com>
2024-12-07 18:14:24 +0300

    Update ContactController.java
    commit maded at home

--- src/main/java/ru/StudentDB/studentDB/controllers/ContactController.java
+++ src/main/java/ru/StudentDB/studentDB/controllers/ContactController.java
@@ -2,6 +2,7 @@ package ru.StudentDB.studentDB.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.StudentDB.studentDB.models.Contact;
 import ru.StudentDB.studentDB.services.ContactService;
@@ -14,11 +15,13 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @GetMapping("/contacts")
-    public List<Contact> getAllContacts() {
-        return contactService.getAllContacts();}
+    @GetMapping
+    @Validated
+    public ResponseEntity<List<Contact>> getAllContacts() {
+        return ResponseEntity.ok(contactService.getAllContacts());
+    }
 
-    @GetMapping("/contacts/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Contact> getContactById(@PathVariable Long id) {
         return contactService.getContactById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
@@ -30,11 +33,7 @@ public class ContactController {
 
     @PutMapping("/contacts/{id}")
     public ResponseEntity<Contact> updateContact(@PathVariable Long id, @RequestBody Contact updatedContact) {
-        return contactService.getContactById(id).map(contact -> {
-            contact.setName(updatedContact.getName());
-            contact.setEmail(updatedContact.getEmail());
-            return ResponseEntity.ok(contactService.createContact(contact));
-        }).orElse(ResponseEntity.notFound().build());
+        return contactService.updateContact(id, updatedContact);
     }
 }
 
